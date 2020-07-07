372333ec8e742adda2bdb724c2f1d47f71eb64b9
Yago_Sevatarion <berrserk.s@gmail.com>
2020-07-07 10:03:44 +0300

    updateTowork

--- MyKernelMod/lkm_example.c
+++ MyKernelMod/lkm_example.c
@@ -8,11 +8,7 @@ MODULE_DESCRIPTION("A simple example Linux modul.");
 MODULE_VERSION("0.01");
 
 static int __init lkm_example_init(void) {
-<<<<<<< HEAD
   printk(KERN_INFO "Hello, World!\n");
-=======
-  printk(KERN_INFO “Hello, World!\n”);
->>>>>>> parent of 13c926e... stg
   return 0;
 }
 static void __exit lkm_example_exit(void) {
